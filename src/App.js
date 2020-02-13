896b5db9b20de5a8eb268711d1f0c01196ded286
Maryame <faitah.maryame@gmail.com>
2020-02-13 11:18:03 +0100

    commit exo1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {
-  BrowserRouter  as  Router,
+  BrowserRouter as Router,
   Switch,
   Route,
   Link,
-} from  'react-router-dom';
-import Header from './header.jsx';
-import './header.css';
+} from 'react-router-dom';
 import Exo1 from './exo1.jsx';
+import './exo1.css';
 import Exo2 from './exo2.jsx';
+import './exo2.css';
 import Exo3 from './exo3.jsx';
 import Exo4 from './exo4.jsx';
 import Exo5 from './exo5.jsx';
@@ -17,28 +17,40 @@ import Exo7 from './exo7.jsx';
 import './App.css';
 import './../node_modules/bootstrap/dist/css/bootstrap.css';
 
-
 function App() {
   return (
-    <div className="App App-header">
+    <div className="App">
       <Router>
         <div>
+          <div className="bg-light">
+            <p className="titre p-2 text-center">Javascript DOM</p>
+          </div>
           <nav>
-            <ul>
-              <li><Link to="/Header">Header</Link></li>
-              <li><Link to="/Exo1">Exo1</Link></li>
-              <li><Link to="/Exo2">Exo2</Link></li>
-              <li><Link to="/Exo3">Exo3</Link></li>
-              <li><Link to="/Exo4">Exo4</Link></li>
-              <li><Link to="/Exo5">Exo5</Link></li>
-              <li><Link to="/Exo6">Exo6</Link></li>
-              <li><Link to="/Exo7">Exo7</Link></li>
+            <ul className="nav nav-pills justify-content-center">
+              <div className="li">
+              <li><Link to="/Exo1" className="text-light text-decoration-none">Exercice 1</Link></li>
+              </div>
+              <div className="li">
+              <li><Link to="/Exo2" className="text-light text-decoration-none">Exercice 2</Link></li>
+              </div>
+              <div className="li">
+              <li><Link to="/Exo3" className="text-light text-decoration-none">Exercice 3</Link></li>
+              </div>
+              <div className="li">
+              <li><Link to="/Exo4" className="text-light text-decoration-none">Exercice 4</Link></li>
+              </div>
+              <div className="li">
+              <li><Link to="/Exo5" className="text-light text-decoration-none">Exercice 5</Link></li>
+              </div>
+              <div className="li">
+              <li><Link to="/Exo6" className="text-light text-decoration-none">Exercice 6</Link></li>
+              </div>
+              <div className="li">
+              <li><Link to="/Exo7" className="text-light text-decoration-none">Exercice 7</Link></li>
+              </div>
             </ul>
           </nav>
           <Switch>
-            <Route path="/Header">
-              <Header></Header>
-            </Route>
             <Route path="/Exo1">
               <Exo1></Exo1>
             </Route>
--- src/exo1.jsx
+++ src/exo1.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-function Exo1 () {
-    return(
+let Exo1 = () => {
+    let modificationExo1 = ()=> {
+        let title = document.getElementById('title');
+        title.innerHTML = "  Mon titre modifié";
+        title.classList.add("text-primary");
+        title.classList.add('bg-info');
+    }
+    return (
         <div>
-            <h1>Hey</h1>
+            <h2 id="title" onClick={()=>modificationExo1()} className="bg-secondary text-white text-left mx-5 mt-5">
+                Mon titre normal
+            </h2>
+            <div className="mx-5">
+                <p>Liste des propriétés et méthodes à retrouver dans la documentation:</p>
+                <ul>
+                    <li className="text-decoration-none">Inner Text</li>
+                    <li className="text-decoration-none">Inner HTML</li>
+                    <li className="text-decoration-none">querySelector()</li>
+                    <li className="text-decoration-none">querySelectorAll()</li>
+                    <li className="text-decoration-none">style</li>
+                    <li className="text-decoration-none">classList</li>
+                </ul>
+                <p>Changer le contenu du h2 par "le titre modifié" </p>
+                <p>Changer la couleur du h2 (par ce que vous voulez)</p>
+                <p>Ajouter une class background bootstrap au h2</p>
+            </div>
+
         </div>
     )
+
 }
 
 export default Exo1;
+
